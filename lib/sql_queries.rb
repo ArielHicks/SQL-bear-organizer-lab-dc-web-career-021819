2d75b04f2927117518a717b4877272bc31e99ef6
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-12 18:00:10 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,26 +6,4 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT bears.name FROM bears ORDER BY bears.name ASC"
 end
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
-end
-
-def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
-end
-
-def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
-end
-
-def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
-end
-
-def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
-end
-
-def selects_bear_that_killed_Tim
-  "Write your SQL query here"
-end
+SELECT bears.name, bears.age FROM bears WHERE gender='F';
