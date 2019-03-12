37f7dd2ed5c3d230f32efd037c36d5fd0ec7ea15
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-12 17:57:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1, 'Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears VALUES (2, 'Rowdy', 10, 'F', 'purple', 'happy', 'true')
-INSERT INTO bears VALUES (3, 'Tabitha', 5, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears VALUES (4, 'Sergeant Brown', 8, 'F', 'black', 'unfettered', 'true')
-INSERT INTO bears VALUES (5, 'Melissa', 2, 'M', 'orange', 'excited', 'true')
-INSERT INTO bears VALUES (6, 'Grinch', 3, 'F', 'white', 'silly', 'true')
-INSERT INTO bears VALUES (7, 'Wendy', 9, 'M', 'white and black', 'ashamed', 'false')
-INSERT INTO bears VALUES (8, null, 2, 'F', 'calico', 'stoked', 'false')
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true');
+INSERT INTO bears VALUES (2, 'Rowdy', 10, 'F', 'purple', 'happy', 'true');
+INSERT INTO bears VALUES (3, 'Tabitha', 5, 'M', 'brown', 'angry', 'true');
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 8, 'F', 'black', 'unfettered', 'true');
+INSERT INTO bears VALUES (5, 'Melissa', 2, 'M', 'orange', 'excited', 'true');
+INSERT INTO bears VALUES (6, 'Grinch', 3, 'F', 'white', 'silly', 'true');
+INSERT INTO bears VALUES (7, 'Wendy', 9, 'M', 'white and black', 'ashamed', 'false');
+INSERT INTO bears VALUES (8, null, 2, 'F', 'calico', 'stoked', 'false');
