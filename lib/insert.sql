ef717cd541dd74d7a8940426d936111c4422fac0
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-12 17:48:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,6 +6,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'M', 'orange', 'excited', 'true')
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'F', 'white', 'silly', 'true')
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'M', 'white and black', 'ashamed', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (nil, 2, 'F', 'calico', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (nil, 2, 'F', 'calico', 'stoked', 'true')
 
 -- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
