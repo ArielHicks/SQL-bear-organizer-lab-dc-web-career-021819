cf74f87168d12f789296d15e649411cb476703ce
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-12 17:46:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 12, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, 'F', 'purple', 'happy', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 12, 'M', 'brown', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'brown', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 12, 'M', 'brown', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 12, 'M', 'brown', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 12, 'M', 'brown', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (nil, 12, 'M', 'brown', 'angry', 'true')
 
 -- Tabitha
 -- Sergeant Brown
