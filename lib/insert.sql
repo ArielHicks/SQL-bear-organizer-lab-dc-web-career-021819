d25f6c22436ae47d306fe43ad5a614842de75208
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-12 17:47:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,10 +2,10 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Ch
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, 'F', 'purple', 'happy', 'true')
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'M', 'brown', 'angry', 'true')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (nil, 2, 'M', 'brown', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'F', 'black', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'M', 'orange', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'F', 'white', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'M', 'white and black', 'angry', 'true')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (nil, 2, 'F', 'calico', 'angry', 'true')
 
 -- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
